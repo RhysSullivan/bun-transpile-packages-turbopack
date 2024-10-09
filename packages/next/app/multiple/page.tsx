97c93209190084301755c82b199bd65bcbb101f5
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-08 22:21:48 -0700

    latest

--- packages/next/app/multiple/page.tsx
+++ packages/next/app/multiple/page.tsx
@@ -1,5 +1,5 @@
 /** Add your relevant code here for the issue to reproduce */
-import { HelloWorld } from "@acme/internal/demo";
+import { HelloWorld } from "@acme/internal-multiple/demo";
 import React from "react";
 export default function Home() {
   return <>{HelloWorld()}</>;
---
+++ packages/next/app/single/page.tsx
@@ -0,0 +1,6 @@
+/** Add your relevant code here for the issue to reproduce */
+import { HelloWorld } from "@acme/internal-single/demo";
+import React from "react";
+export default function Home() {
+  return <>{HelloWorld()}</>;
+}
