dd77f23a873242b9d4b8a563cf9b51fbd7338ff3
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-09 09:46:27 -0700

    fix next config

--- packages/next/next.config.mjs
+++ packages/next/next.config.mjs
@@ -2,6 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  transpilePackages: ["@acme/internal"],
+  transpilePackages: ["@acme/internal-single", "@acme/internal-multiple"],
   reactStrictMode: true,
 };
+
+export default nextConfig;
